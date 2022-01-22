a1308a5528904017a534023490bca68e3bb086de
Tariq Ali <tariqali@Tariqs-MacBook-Pro.local>
2022-01-22 09:53:01 +0300

    edit

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-import express from 'express'
-import history from 'connect-history-api-fallback'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
-import fileUpload from 'express-fileupload'
+import express from "express"
+import history from "connect-history-api-fallback"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 
-import AdminRoute from './routes/admin.routes.js'
-import GroupRoute from './routes/group.routes.js'
-import HallRoutes from './routes/hall.routes.js'
-import TeacherRoutes from './routes/teacher.routes.js'
-import TeacherHallsRoutes from './routes/teacherHalls.routes.js'
-import RoleRoutes from './routes/role.routes.js'
+import AdminRoute from "./routes/admin.routes.js"
+import GroupRoute from "./routes/group.routes.js"
+import HallRoutes from "./routes/hall.routes.js"
+import TeacherRoutes from "./routes/teacher.routes.js"
+import TeacherHallsRoutes from "./routes/teacherHalls.routes.js"
+import RoleRoutes from "./routes/role.routes.js"
 
 const App = express()
 dotenv.config()
@@ -20,11 +18,6 @@ App.use(cors())
 App.use(cookieParser())
 App.use(express.json())
 App.use(express.urlencoded({ extended: false }))
-App.use(bodyParser.json());
-App.use(bodyParser.urlencoded({extended: true}));
-App.use(fileUpload({
-    createParentPath: true
-}));
 
 AdminRoute(App)
 GroupRoute(App)
@@ -34,12 +27,12 @@ TeacherHallsRoutes(App)
 RoleRoutes(App)
 
 App.use(
-    history({
-        disableDotRule: true,
-        verbose: true
-    })
+	history({
+		disableDotRule: true,
+		verbose: true
+	})
 )
 
 App.listen(process.env.PORT || 6789, () => {
-    console.log(`Server Running on http://localhost:${process.env.PORT || 5000}`)
+	console.log(`Server Running on http://localhost:${process.env.PORT || 5000}`)
 })
