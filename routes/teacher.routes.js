aeb6093812884b8ba76af7b665817ace2059bddf
Tariq Ali <tariqali@Tariqs-MacBook-Pro.local>
2022-01-23 12:44:06 +0300

    try reading excel

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,12 +1,12 @@
-import { create, update, find, remove , findOne, counts} from '../controllers/teacher.controller.js';
+import { create, update, find, remove , findOne, counts} from "../controllers/teacher.controller.js"
 
 const TeacherRoutes = (app) => {
-    app.post('/api/teachers/add', create)
-    app.patch('/api/teacher/:id', update)
-    app.delete('/api/teacher/:id', remove)
-    app.get('/api/teachers/', find)
-    app.get('/api/teacher/:id', findOne)
-    app.get('/api/countTeachers/', counts)
+	app.post("/api/teachers/add", create)
+	app.patch("/api/teacher/:id", update)
+	app.delete("/api/teacher/:id", remove)
+	app.get("/api/teachers/", find)
+	app.get("/api/teacher/:id", findOne)
+	app.get("/api/countTeachers/", counts)
 }
 
-export default TeacherRoutes;
+export default TeacherRoutes
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import history from "connect-history-api-fallback"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import fileUpload from "express-fileupload"
+import path from "path"
+import xlsxFile from "read-excel-file"
+
+const __dirname = path.resolve()
 
 
 import AdminRoute from "./routes/admin.routes.js"
@@ -19,6 +24,8 @@ App.use(cookieParser())
 App.use(express.json())
 App.use(express.urlencoded({ extended: false }))
 
+App.use(fileUpload())
+
 AdminRoute(App)
 GroupRoute(App)
 HallRoutes(App)
@@ -32,6 +39,43 @@ App.use(
 		verbose: true
 	})
 )
+
+
+function generateRandomName(length, patientId) {
+	var result = ""
+	var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	var charactersLength = characters.length
+	for (var i = 0; i < length; i++) {
+		result +=
+            characters.charAt(Math.floor(Math.random() * charactersLength)) +
+            patientId
+	}
+	return result
+}
+
+App.post("/api/upload-teacher", async (req, res) => {
+	if (!req.files || Object.keys(req.files).length === 0) {
+		return res.status(400).send("No files were uploaded.")
+	}
+
+	else {
+		let uploadedFile = req.files.excel
+		let photoName = generateRandomName(5, 3)
+		var filename = uploadedFile.name
+		var ext = filename.substr(filename.lastIndexOf(".") + 1)
+		let imagePath = `${__dirname}/attachments/${photoName}.${ext}`
+
+		uploadedFile.mv(imagePath, function (err) {
+			if (err) return res.status(500).send(err)
+			console.log(xlsxFile(imagePath))
+			xlsxFile(uploadedFile).then((rows) => {
+				console.log(rows)
+				res.send(rows)
+			})
+		})
+	}
+})
 
 App.listen(process.env.PORT || 6789, () => {
 	console.log(`Server Running on http://localhost:${process.env.PORT || 5000}`)
